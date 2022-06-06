c77cf0f80ad6e0dc344c8306dcb3b6507095a3bd
VictoKu1 <kushnir.victor@gmail.com>
2022-06-07 01:48:58 +0300

    .

--- shell1.c
+++ shell1.c
@@ -7,47 +7,51 @@
 #include "unistd.h"
 #include <string.h>
 
-int main() {
-int i, amper, retid, status;
-char *argv[10];
-char command[1024];
-char *token;
+int main()
+{
+    int i, amper, retid, status;
+    char *argv[10];
+    char command[1024];
+    char *token;
 
-while (1) {
-    printf("hello: ");
-    fgets(command, 1024, stdin);
-    command[strlen(command) - 1] = '\0'; // replace \n with \0
-
-    /* parse command line */
-    i = 0;
-    token = strtok (command," ");
-    while (token != NULL)
+    while (1)
     {
-        argv[i] = token;
-        token = strtok (NULL, " ");
-        i++;
-    }
-    argv[i] = NULL;
+        printf("hello: ");
+        fgets(command, 1024, stdin);
+        command[strlen(command) - 1] = '\0'; // replace \n with \0
 
-    /* Is command empty */ 
-    if (argv[0] == NULL)
-        continue;
-
-    /* Does command line end with & */ 
-    if (! strcmp(argv[i - 1], "&")) {
-        amper = 1;
-        argv[i - 1] = NULL;
+        /* parse command line */
+        i = 0;
+        token = strtok(command, " ");
+        while (token != NULL)
+        {
+            argv[i] = token;
+            token = strtok(NULL, " ");
+            i++;
         }
-    else 
-        amper = 0; 
+        argv[i] = NULL;
 
-    /* for commands not part of the shell command language */ 
+        /* Is command empty */
+        if (argv[0] == NULL)
+            continue;
 
-    if (fork() == 0) { 
-        execvp(argv[0], argv);
+        /* Does command line end with & */
+        if (!strcmp(argv[i - 1], "&"))
+        {
+            amper = 1;
+            argv[i - 1] = NULL;
+        }
+        else
+            amper = 0;
+
+        /* for commands not part of the shell command language */
+
+        if (fork() == 0)
+        {
+            execvp(argv[0], argv);
+        }
+        /* parent continues here */
+        if (amper == 0)
+            wait(NULL);
     }
-    /* parent continues here */
-    if (amper == 0)
-        wait(NULL);
-}
 }
--- shell2.c
+++ shell2.c
@@ -10,23 +10,29 @@
 
 char name[1024];
 
-void intHandler(int n) {
+void intHandler(int n)
+{
     printf("\nYou typed Control-C!\n");
 }
 
-int charInStr(char* str, char c, int size){
+int charInStr(char *str, char c, int size)
+{
     int count = 0;
-    for(int i = 0; str[i] && i < size; i++){
-        if(str[i] == c){
+    for (int i = 0; str[i] && i < size; i++)
+    {
+        if (str[i] == c)
+        {
             count++;
         }
     }
     return count;
 }
 
-int main() {
+int main()
+{
     strcpy(name, "hello");
-    if(signal(SIGINT, intHandler) == SIG_ERR){
+    if (signal(SIGINT, intHandler) == SIG_ERR)
+    {
         printf("SIGINT ERROR");
     }
     char command[1024], prev_command[1024];
@@ -46,46 +52,53 @@ int main() {
         command[strlen(command) - 1] = '\0';
 
         // !!
-        if(strcmp(command,"!!")){
+        if (strcmp(command, "!!"))
+        {
             strcpy(prev_command, command);
         }
-        else{
+        else
+        {
             strcpy(command, prev_command);
         }
 
         pipes_num = charInStr(command, '|', 1024);
-        if(pipes_num > 0){
-            pargv = (char***)(malloc(pipes_num * sizeof(char***)));
-            for(int r = 0; r < pipes_num; r++){
-                pargv[r] = (char**)(malloc(10 * sizeof(char*)));
+        if (pipes_num > 0)
+        {
+            pargv = (char ***)(malloc(pipes_num * sizeof(char ***)));
+            for (int r = 0; r < pipes_num; r++)
+            {
+                pargv[r] = (char **)(malloc(10 * sizeof(char *)));
             }
         }
-        
+
         /* parse command line */
         i = 0;
-        token = strtok (command," ");
+        token = strtok(command, " ");
         while (token != NULL)
         {
             argv[i] = token;
-            token = strtok (NULL, " ");
+            token = strtok(NULL, " ");
             i++;
-            if (token && ! strcmp(token, "|")) {
+            if (token && !strcmp(token, "|"))
+            {
                 break;
             }
         }
         argv[i] = NULL;
 
-        if(pipes_num > 0){
-            token = strtok (NULL, " ");
+        if (pipes_num > 0)
+        {
+            token = strtok(NULL, " ");
             pi = 0;
             pj = 0;
             while (token != NULL)
             {
                 pargv[pi][pj] = token;
-                token = strtok (NULL, " ");
+                token = strtok(NULL, " ");
                 pj++;
-                if(token && ! strcmp(token, "|")){
-                    token = strtok (NULL, " ");
+                if (token && !strcmp(token, "|"))
+                {
+                    token = strtok(NULL, " ");
                     pargv[pi][pj] = NULL;
                     pi++;
                     pj = 0;
@@ -97,48 +110,58 @@ int main() {
         if (argv[0] == NULL)
             continue;
 
-        /* Does command line end with & */ 
-        if (! strcmp(argv[i - 1], "&")) {
+        /* Does command line end with & */
+        if (!strcmp(argv[i - 1], "&"))
+        {
             amper = 1;
             argv[i - 1] = NULL;
         }
-        else {
-            amper = 0; 
+        else
+        {
+            amper = 0;
         }
-        if (i > 2 && ! strcmp(argv[i - 2], ">")) {
+        if (i > 2 && !strcmp(argv[i - 2], ">"))
+        {
             redirect = 1;
             argv[i - 2] = NULL;
             outfile = argv[i - 1];
         }
-        else if(i > 2 && !strcmp(argv[i - 2], "2>")){
+        else if (i > 2 && !strcmp(argv[i - 2], "2>"))
+        {
             redirect = 2;
             argv[i - 2] = NULL;
             outfile = argv[i - 1];
         }
-        else {
-            redirect = 0; 
+        else
+        {
+            redirect = 0;
         }
         /* built in commands */
 
         // =
-        if(i > 2 && ! strcmp(argv[0], "prompt") && ! strcmp(argv[1], "=")){
+        if (i > 2 && !strcmp(argv[0], "prompt") && !strcmp(argv[1], "="))
+        {
             strcpy(name, argv[2]);
             continue;
         }
-        
+
         // echo
-        if(! strcmp(argv[0], "echo")){
+        if (!strcmp(argv[0], "echo"))
+        {
             j = 1;
             while (argv[j])
             {
-                if(!strcmp(argv[j], "$?")){
+                if (!strcmp(argv[j], "$?"))
+                {
                     printf("%d", status);
                 }
-                else{
+                else
+                {
                     printf("%s", argv[j]);
                 }
                 j++;
-                if(argv[j]){
+                if (argv[j])
+                {
                     printf(" ");
                 }
             }
@@ -147,47 +170,58 @@ int main() {
         }
 
         // cd
-        if(! strcmp(argv[0], "cd"))
+        if (!strcmp(argv[0], "cd"))
         {
-            if(!argv[1]){
+            if (!argv[1])
+            {
                 chdir(getenv("HOME"));
             }
-            else if(chdir(argv[1]) == -1){
+            else if (chdir(argv[1]) == -1)
+            {
                 printf("No such file or directory\n");
             }
             continue;
         }
 
         // quit
-        if(! strcmp(argv[0], "quit")){
+        if (!strcmp(argv[0], "quit"))
+        {
             exit(0);
         }
 
-        /* for commands not part of the shell command language */ 
+        /* for commands not part of the shell command language */
 
-        if (fork() == 0) { 
+        if (fork() == 0)
+        {
             /* redirection of IO ? */
-            if (redirect) {
-                fd = creat(outfile, 0660); 
-                close (redirect) ; 
-                dup(fd); 
-                close(fd); 
+            if (redirect)
+            {
+                fd = creat(outfile, 0660);
+                close(redirect);
+                dup(fd);
+                close(fd);
             }
-            if(pipes_num > 0){
-                for(int r = 0; r < pipes_num; r++){
+            if (pipes_num > 0)
+            {
+                for (int r = 0; r < pipes_num; r++)
+                {
                     pipe(fildes[r]);
-                    if(fork() == 0){
+                    if (fork() == 0)
+                    {
                         close(STDOUT_FILENO);
                         dup(fildes[r][1]);
                         close(fildes[r][1]);
                         close(fildes[r][0]);
-                        if(r == 0){
-                            if(execvp(argv[0], argv) == -1)
+                        if (r == 0)
+                        {
+                            if (execvp(argv[0], argv) == -1)
                                 printf("command %s not found\n", pargv[r][0]);
-                                exit(0);
+                            exit(0);
                         }
-                        else{ 
-                            if(execvp(pargv[r - 1][0], pargv[r - 1]) == -1){
+                        else
+                        {
+                            if (execvp(pargv[r - 1][0], pargv[r - 1]) == -1)
+                            {
                                 printf("command %s not found\n", pargv[r - 1][0]);
                                 exit(0);
                             }
@@ -197,23 +231,28 @@ int main() {
                     dup(fildes[r][0]);
                     close(fildes[r][0]);
                     close(fildes[r][1]);
-                    if(r == pipes_num - 1 && execvp(pargv[r][0], pargv[r]) == -1){
+                    if (r == pipes_num - 1 && execvp(pargv[r][0], pargv[r]) == -1)
+                    {
                         printf("command %s not found\n", pargv[r][0]);
                         exit(0);
                     }
                 }
             }
-            else if(execvp(argv[0], argv) == -1){
+            else if (execvp(argv[0], argv) == -1)
+            {
                 printf("command %s not found\n", argv[0]);
                 exit(0);
             }
         }
         /* parent continues here */
-        if (amper == 0){
+        if (amper == 0)
+        {
             retid = wait(&status);
         }
-        if(pipes_num > 0){
-            for(int r = 0; r < pipes_num; r++){
+        if (pipes_num > 0)
+        {
+            for (int r = 0; r < pipes_num; r++)
+            {
                 free(pargv[r]);
             }
             free(pargv);
